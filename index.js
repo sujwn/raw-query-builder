2509b16a5410101d21fa283f1eab4352e16f6eee
Suhlani Juwan <jhubeishan@gmail.com>
2023-10-28 12:43:52 +0700

    remove example

--- index.js
+++ index.js
@@ -16,12 +16,3 @@ exports.generateMany = function (query) {
 exports.info = function () {
     console.log("Raw Query Builder version 1.0 by SSONG");
 }
-
-const a = this.generateOne({
-    select: ["*"], from: { source: "user" },
-    join: [
-        { left_join: "posts", as: "p", on: "p.user_id = u.id" },
-        { right_join: "address", as: "a", on: "a.user_id = u.id" },
-        { join: "address", as: "a", on: "a.user_id = u.id" }
-    ]
-});
