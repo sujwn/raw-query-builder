6fd81d9d8d97bf58a5810ea64d0d7f095440846a
Suhlani Juwan <jhubeishan@gmail.com>
2023-11-01 23:28:28 +0700

    remove example

--- index.js
+++ index.js
@@ -16,27 +16,3 @@ exports.generateMany = function (query) {
 exports.info = function () {
     console.log("Raw Query Builder version 1.0 by SSONG");
 }
-
-const q = {
-    select: ["*", "COUNT(p.*) AS post_count"],
-    from: { source: 'user', as: 'u' },
-    join: [
-        {
-            left_join: 'post', as: 'p', on: 'p.user_id = u.id'
-        }
-    ],
-    where: [
-        { and: 'id > 10' },
-        { or: 'id <= 100'},
-        { and: 'status is true' }
-    ],
-    group: ["u.id"],
-    sort: [
-        ["u.name", "asc"],
-        ["u.createdAt", "asc"]
-    ],
-    limit: 50,
-    offset: 0
-}
-
-this.generateMany(q)
